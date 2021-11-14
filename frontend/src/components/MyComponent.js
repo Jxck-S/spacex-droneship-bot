995345254ac032ea739fc03933dcc62b157f3cac
dmorozbustos <dmorozbustos@knights.ucf.edu>
2021-11-13 20:25:39 -0500

    Added fetch function

---
+++ frontend/src/components/MyComponent.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: 28.621298,
+  lng: -81.2964718
+};
+
+function MyComponent() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyBytw8OGq-Rl19P1OCkBvi1nSxzqJr6F5w"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={16}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
+}
+
+export default React.memo(MyComponent)
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,25 +1,29 @@
 import React from 'react'
 // import App from '../App'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import DirectionsBoatIcon from '@mui/icons-material/DirectionsBoat';
 
 import {
-    // Box,
+    Box,
     AppBar,
     Toolbar,
-    Button,
-    Typography
+    // Button,
+    Typography,
+    IconButton
 } from '@mui/material'
 
 
 const Navbar = () => {
     return (
-        <>
-            <AppBar sx={{bgcolor: "secondary.main", borderRadius: '0  0 .5rem .5rem'}}>
-                <Toolbar>
-                    <Typography color="#333">SpaxeX Droneship Bot Dashboard</Typography>
-                    <Button variant="contained">Test Button</Button>
+        <Box sx = {{mx: '5rem'}}>
+            <AppBar position="relative" sx={{bgcolor: "secondary.main", borderRadius: '0  0 .5rem .5rem', margin: '0 1rem'}}>
+                <Toolbar sx={{display:'flex', justifyContent:'space-between'}}>
+                    <IconButton color="primary" href="/"><DirectionsBoatIcon /></IconButton>
+                    <Typography sx={{fontWeight: 800}} color="#333">SpaxeX Droneship Bot</Typography>
+                    <IconButton color="primary" href="https://github.com/Jxck-S/spacex-droneship-bot"><GitHubIcon /></IconButton>
                 </Toolbar>
             </AppBar>
-        </>
+        </Box>
     )
 }
 
